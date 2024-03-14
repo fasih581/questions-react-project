61a0e3c6d8bc77088af1aa63be9c423ce814dd6a
fasih_ek <fasihedakkudi@gmail.com>
2024-03-14 13:25:17 +0530

    first commit

--- API/server/controller/registration.controller.js
+++ API/server/controller/registration.controller.js
@@ -22,27 +22,3 @@ exports.RegUserPost = asyncHandler(async (req, res) => {
     console.log(error);
   }
 });
-
-
-// exports.postBlog = asyncHandler(async (req, res) => {
-//   const data = new blogModel({
-//     blogImg: req.body.blogImg,
-//     title: req.body.title,
-//     subTitle: req.body.subTitle,
-//     description: req.body.description,
-//     month: req.body.month,
-//   });
-
-//   data
-//     .save()
-//     .then((data) => {
-//       res.status(201).json(data);
-//     })
-//     .catch((err) => {
-//       console.error("Error Creating Blog:", err);
-//       res.status(500).json({
-//         message: "Internal server error creating blog",
-//         error: err.message || "Internal Server Error",
-//       });
-//     });
-// });
